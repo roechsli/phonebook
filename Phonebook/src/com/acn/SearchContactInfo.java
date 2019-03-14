1dc5b9b547e56832396dd6c60b8c2bbdf8f343cf
roechsli <romanoech@gmail.com>
2019-03-14 15:49:20 +0100

    structure

--- Phonebook/src/com/acn/SearchContactInfo.java
+++ Phonebook/src/com/acn/SearchContactInfo.java
@@ -9,7 +9,7 @@ public class SearchContactInfo {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		// initialize map
-		
+		int marius = 1000;
 		Map<String, Person> myMap = initMap();
 		// define and add some random characters
 		// felix
@@ -19,6 +19,8 @@ public class SearchContactInfo {
 		
 		// search for specific phone number in map
 		// roman
+		String numAsStr = "+417575757575";
+		searchByNumber(numAsStr);
 		
 		
 		
@@ -32,6 +34,11 @@ public class SearchContactInfo {
 		// 3. search phone number if found: printout phone number of contact
 		
 		// 4. printout Name: and wait for input
+	}
+
+	private static void searchByNumber(Map phonebook, String numAsStr) {
+		System.out.println("Person not found");
+		
 	}
 
 	private static Map<String, Person> initMap() {
