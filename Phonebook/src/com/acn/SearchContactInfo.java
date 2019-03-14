ed5e080705ecae280e5ff48b564122f029e09ee2
roechsli <romanoech@gmail.com>
2019-03-14 16:54:02 +0100

    Revert "trial for merging"
    This reverts commit 05391b127674866ae6f529a5c1b1b0abcf2490f5, reversing
    changes made to a48416be70cd024c9398c7f67ffb9cad2d272f4b.

---
+++ Phonebook/src/com/acn/SearchContactInfo.java
@@ -0,0 +1,71 @@
+package com.acn;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class SearchContactInfo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// initialize map
+		
+		Map<String, Person> myMap = initMap();
+		
+		// define and add some random characters
+		// felix
+
+		myMap.put("roman", new Person("roman", "ederstr.12", "0190123456"));
+
+		Person dubbl = new Person("Donald Duck", "Entenhausen", "0815");
+		myMap.put(dubbl.name, dubbl);
+
+
+		// search for specific phone number in map
+		// roman
+		String numAsStr = "+417575757575";
+		searchByNumber(myMap, numAsStr);
+		numAsStr = "0815";
+		searchByNumber(myMap, numAsStr);
+		searchByName(myMap, "Donald Duck");
+		
+		
+		// start app in console with Name: and wait for user input
+		// after input set with enter:
+		// 1. if contact does not exist: print contact does not exist
+		// 2. search address if found: printout address
+
+		// 3. search phone number if found: printout phone number of contact
+
+		// 4. printout Name: and wait for input
+	}
+
+	
+	private static void searchByNumber(Map phonebook, String numAsStr) {
+		System.out.println("Searching for number:" + numAsStr);
+		for (Object obj : phonebook.values()) {
+			Person pers = (Person) obj;
+			System.out.println(obj);
+			System.out.println(pers.name);
+			if (pers.phoneNumber.equals(numAsStr)) {
+				// found the person
+				System.out.println("The number belongs to: "+pers.name);
+				return;
+			}
+		}
+		System.out.println("Couldn't find a person with this number");
+	}
+
+	private static void searchByName(Map p1, String str1) {
+		for(int i = 0; i < p1.size();i++) {
+			if(p1.containsValue(str1)) { System.out.println("Person gefunden:" + str1);}
+		} System.out.println(str1 + " wurde nicht gefunden");
+		}
+
+	private static Map<String, Person> initMap() {
+		// First String is name
+		// Second element is person info
+		Map<String, Person> phonebook = new HashMap<String, Person>();
+		return phonebook;
+	}
+
+}
