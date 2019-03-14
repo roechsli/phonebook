247b9a4dd30b3f42b1a7f75bc81676b32d4f19dc
roechsli <romanoech@gmail.com>
2019-03-14 14:37:18 +0100

    text

--- Phonebook/src/com/acn/SearchContactInfo.java
+++ Phonebook/src/com/acn/SearchContactInfo.java
@@ -4,7 +4,15 @@ public class SearchContactInfo {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		// initialize map
+		// start app in console with Name: and wait for user input
+		// after input set with enter:
+		// 1. if contact does not exist: print contact does not exist
+		// 2. search address if found: printout address
+		
+		// 3. search phone nmber if found: printout phone number of contact
+		
+		// 4. printout Name: and wait for input
 	}
 
 }
