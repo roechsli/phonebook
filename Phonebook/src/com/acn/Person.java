16c43dd7c1992d2a25c108c29180e05bc6eb7686
roechsli <romanoech@gmail.com>
2019-03-14 15:41:05 +0100

    divide to conquer

---
+++ Phonebook/src/com/acn/Person.java
@@ -0,0 +1,7 @@
+package com.acn;
+
+public class Person {
+
+	
+
+}
--- Phonebook/src/com/acn/SearchContactInfo.java
+++ Phonebook/src/com/acn/SearchContactInfo.java
@@ -1,21 +1,46 @@
 package com.acn;
 
+import java.util.HashMap;
+import java.util.Map;
+
+
 public class SearchContactInfo {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		// initialize map
+		
+		Map<String, Person> myMap = initMap();
+		// define and add some random characters
+		// felix
+		
+		// search for specific character in map
+		// marius
+		
+		// search for specific phone number in map
+		// roman
+		
+		
+		
+		
+		
 		// start app in console with Name: and wait for user input
 		// after input set with enter:
 		// 1. if contact does not exist: print contact does not exist
 		// 2. search address if found: printout address
 		
-		// 3. search phone nmber if found: printout phone number of contact
+		// 3. search phone number if found: printout phone number of contact
 		
 		// 4. printout Name: and wait for input
-		
-		
-		// test
 	}
+
+	private static Map<String, Person> initMap() {
+		// First String is name
+		// Second element is person info
+		Map<String, Person> phonebook = new HashMap<String, Person>();
+		return phonebook;
+	}
+	
+		
 
 }
